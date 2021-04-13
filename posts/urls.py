c6ab74833e5ecfa376be91b42d7e9e1c36c1c334
Ivan Safronov <60986353+issafronov@users.noreply.github.com>
2021-04-13 17:32:13 +0300

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
         views.post_edit,
         name="post_edit"
     ),
-    path("<str:username>/<int:post_id>/comment",
+    path("<str:username>/<int:post_id>/comment/",
          views.add_comment, name="add_comment"),
     path(
         "<str:username>/follow/", views.profile_follow, name="profile_follow"
